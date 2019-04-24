f6556cfef7c38e4c77984aeef6bbb85886513026
Rustem Shaydullin <ndiezel0@gmail.com>
2019-04-24 15:59:23 +0300

    CDS-81: split cds into storage and keyring and remove dmsl

--- proto/cds.thrift
+++ proto/cds.thrift
@@ -1,6 +1,4 @@
-include "proto/domain.thrift"
-
-namespace java com.rbkmoney.cds.cds
+namespace java com.rbkmoney.cds.keyring
 
 /** Часть мастер-ключа */
 typedef binary MasterKeyShare;
@@ -18,56 +16,6 @@ struct EncryptedMasterKeyShare {
 }
 
 typedef list<EncryptedMasterKeyShare> EncryptedMasterKeyShares;
-
-/** Дата экспирации */
-struct ExpDate {
-    /** Месяц 1..12 */
-    1: required i8 month
-    /** Год 2015..∞ */
-    2: required i16 year
-}
-
-/** Открытые карточные данные (в отличие от domain.BankCard) */
-struct CardData {
-    /** Номер карточки без пробелов [0-9]{14,19} */
-    1: required string pan
-    2: required ExpDate exp_date
-    /** Имя держателя */
-    3: optional string cardholder_name
-    /** Deprecated */
-    /** Код верификации [0-9]{3,4} */
-    4: optional string cvv
-}
-
-struct PutCardDataResult {
-    1: required domain.BankCard bank_card
-    2: required domain.PaymentSessionID session_id
-}
-
-/** Код проверки подлинности банковской карты */
-struct CardSecurityCode {
-    /** Код верификации [0-9]{3,4} */
-    1: required string value
-}
-
-/** Данные, необходимые для авторизации по 3DS протоколу */
-struct Auth3DS {
-    /** Криптограмма для проверки подлинности */
-    1: required string cryptogram
-    /** Тип транзакции */
-    2: optional string eci
-}
-
-/** Данные, необходимые для проверки подлинности банковской карты */
-union AuthData {
-    1: CardSecurityCode card_security_code
-    2: Auth3DS auth_3ds
-}
-
-/** Данные сессии */
-struct SessionData {
-    1: required AuthData auth_data
-}
 
 struct Success {}
 
@@ -166,14 +114,6 @@ exception InvalidStatus {
 exception InvalidActivity {
     1: required Activity activity
 }
-
-exception InvalidCardData {
-    1: optional string reason
-}
-
-exception CardDataNotFound {}
-
-exception SessionDataNotFound {}
 
 exception InvalidArguments {
     1: optional string reason
@@ -289,30 +229,4 @@ service Keyring {
 
     /** Отменяет процесс добавления нового ключа в кейринг */
     void CancelRotate () throws (1: InvalidStatus invalid_status)
-}
-
-/**
- * Интерфейс для приложений
- *
- * При недоступности (отсутствии или залоченности) кейринга сервис сигнализирует об этом с помощью
- * woody-ошибки `Resource Unavailable`.
- */
-service Storage {
-
-    /** Получить карточные данные без CVV */
-    CardData GetCardData (1: domain.Token token)
-        throws (1: CardDataNotFound not_found)
-
-    /** Получить карточные данные c CVV */
-    CardData GetSessionCardData (1: domain.Token token, 2: domain.PaymentSessionID session_id)
-        throws (1: CardDataNotFound not_found)
-
-    /** Получить данные сессии */
-    SessionData GetSessionData (1: domain.PaymentSessionID session_id)
-        throws (1: SessionDataNotFound not_found)
-
-    /** Сохранить карточные данные */
-    PutCardDataResult PutCardData (1: CardData card_data, 2: SessionData session_data)
-        throws (1: InvalidCardData invalid)
-
 }
